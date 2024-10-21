50ea01f6e6c4a00e3c04065ec3d2a6d075d9f5d6
Danny Garcia <dgarcia1724@berkeley.edu>
2024-10-20 23:13:36 -0700

    Fresh start

---
+++ src/test/java/danny/leetcode_study_app_backend/LeetcodeStudyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package danny.leetcode_study_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeStudyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
