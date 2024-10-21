40e10772cf9de169582919d246da7de0dfdedd5c
Danny Garcia <dgarcia1724@berkeley.edu>
2024-10-21 00:03:57 -0700

    .env set up and connected to DB

--- src/main/java/danny/leetcode_study_app_backend/LeetcodeStudyAppBackendApplication.java
+++ src/main/java/danny/leetcode_study_app_backend/LeetcodeStudyAppBackendApplication.java
@@ -2,11 +2,18 @@ package danny.leetcode_study_app_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LeetcodeStudyAppBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // Loads .env file
+
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(LeetcodeStudyAppBackendApplication.class, args);
 	}
 
